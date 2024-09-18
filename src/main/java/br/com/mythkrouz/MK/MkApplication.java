2d3369d66f617603ee9c6faa60b43c4392ef5c8b
holyvieri <beatriz.oliveirac@upe.br>
2024-09-18 00:41:35 -0300

    `spring boot` init

---
+++ src/main/java/br/com/mythkrouz/MK/MkApplication.java
@@ -0,0 +1,13 @@
+package br.com.mythkrouz.MK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mythkrouz/MK/MkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mythkrouz.MK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
